10d0d271ad05280d7e4c667efcd833fd74f4a086
François St-Germain <fstgerm@gmail.com>
2017-12-09 20:16:58 -0500

    Day 02 – Part 2

--- 2017/02/challenge.go
+++ 2017/02/challenge.go
@@ -8,35 +8,59 @@ import (
   "strconv"
 )
 
-var total int
+var total1, total2 int
 
 func main() {
   file, _ := os.Open("input.txt")
   fscanner := bufio.NewScanner(file)
 
   for fscanner.Scan() {
-    min, max := getNumbers(fscanner.Text())
-    total += max - min
+    numbers := findNumbers(fscanner.Text())
+    total1 += subtract(numbers)
+    total2 += divide(numbers)
   }
 
-  fmt.Println(total)
+  fmt.Printf("Part 1 : %d\n", total1)
+  fmt.Printf("Part 2 : %d\n", total2)
 }
 
-func getNumbers(text string) (min , max int) {
+func findNumbers(text string) (numbers []int) {
   pattern := regexp.MustCompile("[0-9]+")
   matches := pattern.FindAllString(text, -1)
 
   for _, value := range matches {
-    number, _ := strconv.Atoi(value)
-
-    if number < min || min == 0 {
-      min = number
-    }
-
-    if number > max || max == 0 {
-      max = number
-    }
+    n, _ := strconv.Atoi(value)
+    numbers = append(numbers, n)
   }
 
   return
+}
+
+func subtract(numbers []int) int {
+  min := numbers[0]
+  max := numbers[0]
+
+  for _, value := range numbers {
+    if value < min {
+      min = value
+    }
+
+    if value > max {
+      max = value
+    }
+  }
+
+  return max - min
+}
+
+func divide(numbers []int) int {
+  for _, a := range numbers {
+    for _, b := range numbers {
+      if a % b == 0 && a != b {
+        return a / b
+      }
+    }
+  }
+
+  return 0
 }
