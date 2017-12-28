f04346caab588ff52e8e943e8cf77882a3031bb7
François St-Germain <fstgerm@gmail.com>
2017-12-28 15:07:51 -0500

    Day 04 - Part 2

--- 2017/04/challenge.cpp
+++ 2017/04/challenge.cpp
@@ -10,21 +10,34 @@ int main() {
   ifstream file("input.txt");
   string line;
 
-  int total = 0, duplicates = 0;
+  int total = 0, duplicates = 0, anagrams = 0;
 
   while (getline(file, line)) {
     string word;
     istringstream iss(line);
     map<string, int> occurrences;
+    map<string, int> occurrences_sorted;
     total++;
+
+    bool has_duplicate = false, has_anagram = false;
 
     while (iss >> word) {
       if (++occurrences[word] == 2) {
-        duplicates++;
-        break;
+        has_duplicate = true;
+      }
+
+      std::string sorted_word = word;
+      std::sort(sorted_word.begin(), sorted_word.end());
+
+      if (++occurrences_sorted[sorted_word] == 2) {
+        has_anagram = true;
       }
     }
+
+    if (has_duplicate) duplicates++;
+    if (has_anagram) anagrams++;
   }
 
   cout << "Part 1 : " << total - duplicates << endl;
+  cout << "Part 2 : " << total - anagrams << endl;
 }
