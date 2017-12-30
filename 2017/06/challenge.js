f33c13a81c0f32be9fc2b3a5450bb2a98a9417b3
François St-Germain <fstgerm@gmail.com>
2017-12-29 20:05:59 -0500

    Day 06 – Part 2

--- 2017/06/challenge.js
+++ 2017/06/challenge.js
@@ -2,18 +2,22 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf8');
 const blocks = input.split("\t").map(Number);
 
-class Bank {
+class Banks {
   constructor (blocks) {
     this.blocks = blocks;
     this.states = [this.state()];
+    this.loop_size = 0;
+
     this.redistribute();
   }
 
   redistribute () {
     while (true) {
       let state = this.cycle();
+      let index = this.states.indexOf(state);
 
-      if (this.states.indexOf(state) > -1) {
+      if (index > -1) {
+        this.loop_size = this.states.length - index;
         break;
       }
 
@@ -40,6 +44,7 @@ class Bank {
   }
 }
 
-const bank = new Bank(blocks);
+const banks = new Banks(blocks);
 
-console.log('Part 1 :', bank.states.length);
+console.log('Part 1 :', banks.states.length);
+console.log('Part 2 :', banks.loop_size);
