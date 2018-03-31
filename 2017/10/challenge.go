6c894d3f33b02ea94a273f489188e8dc7be52b45
François St-Germain <fstgerm@gmail.com>
2018-03-31 13:08:16 +0700

    Day 10 - Part 2

--- 2017/10/challenge.go
+++ 2017/10/challenge.go
@@ -24,8 +24,7 @@ func NewSequence(size int) *Sequence {
 }
 
 func (s *Sequence) Move(size int) {
-    s.Position = (s.Position + size + s.Skip) % len(s.Numbers)
-    s.Skip += 1
+    s.Position, s.Skip = (s.Position + size + s.Skip) % len(s.Numbers), s.Skip + 1
 }
 
 func (s *Sequence) Reverse(size int) {
@@ -40,16 +39,45 @@ func (s Sequence) Multiple() int {
 	return s.Numbers[0] * s.Numbers[1]
 }
 
+func (s *Sequence) Hash() (hash string) {
+  for i := 0; i < len(s.Numbers); i += 16 {
+    hash += fmt.Sprintf("%02x", Xor(s.Numbers[i:i+16]))
+  }
+	return
+}
+
+func Xor(numbers []int) (result int) {
+	for _, v := range numbers {
+		result ^= v
+	}
+  return
+}
+
 func main() {
   file, _ := ioutil.ReadFile("input.txt")
-  lengths := strings.Split(strings.TrimSpace(string(file)), ",")
-  sequence := NewSequence(256)
+  input := strings.TrimSpace(string(file))
+  lengths := strings.Split(input, ",")
+  sequence1 := NewSequence(256)
 
   for _, v := range lengths {
     length, _ := strconv.Atoi(v)
-    sequence.Reverse(length)
-    sequence.Move(length)
+    sequence1.Reverse(length)
+    sequence1.Move(length)
   }
 
-  fmt.Println(sequence.Multiple())
+  fmt.Println("Part 1 :", sequence1.Multiple())
+
+  secret := []byte{17, 31, 73, 47, 23}
+  hex := []byte(input + string(secret[:]))
+  sequence2 := NewSequence(256)
+
+  for i := 0; i < 64; i++ {
+    for _, v := range []byte(hex) {
+      length := int(v)
+      sequence2.Reverse(length)
+      sequence2.Move(length)
+    }
+  }
+
+  fmt.Println("Part 2 :", sequence2.Hash())
 }
