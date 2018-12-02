87e9882b4a140333cc37de65b7c43a343c56b4ea
François St-Germain <fstgerm@gmail.com>
2018-12-02 12:50:52 +0700

    Day 01 – Part 2

--- 2018/01/challenge.py
+++ 2018/01/challenge.py
@@ -4,3 +4,13 @@ numbers = map(int, open('input.txt').read().splitlines())
 result = reduce((lambda a, b: a + b), numbers)
 
 print("Part 1: %d" %(result))
+
+i = last = 0
+list = []
+
+while last not in list:
+    list.append(last)
+    last += numbers[i % len(numbers)]
+    i += 1
+else:
+    print("Part 2: %d" %(last))
