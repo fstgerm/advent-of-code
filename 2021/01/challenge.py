f37842c267511fbf4aa1b892556b9c14223ff8d7
François St-Germain <fstgerm@gmail.com>
2021-12-03 22:34:49 -0500

    Day 1 – Part 2

--- 2021/01/challenge.py
+++ 2021/01/challenge.py
@@ -1,6 +1,14 @@
 with open('input.txt') as f:
     depths = [int(line.rstrip()) for line in f]
 
-total = sum([a < b for (a, b) in zip(depths, depths[1:])])
+def get_increase_total(numbers):
+    return sum([a < b for (a, b) in zip(numbers, numbers[1:])])
 
-print(total)
+def get_sum_by_window(numbers, size, step = 1):
+    return [sum(numbers[i:i + size]) for i in range(0, len(numbers), step)]
+
+part1 = get_increase_total(depths)
+part2 = get_increase_total(get_sum_by_window(depths, 3))
+
+print("PART 1 :", part1)
+print("PART 2 :", part2)
